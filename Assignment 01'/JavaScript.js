b4510bd39d5749a9f6251c3fd167aa3d73fc66bf
Alina-mughal <49423581+Alina-mughal@users.noreply.github.com>
2019-04-11 10:51:18 +0500

    Add files via upload

---
+++ Assignment 01'/JavaScript.js
@@ -0,0 +1,91 @@
+﻿function validate() 
+{
+  
+  var name = document.forms["RegForm"]["Name"];
+   
+ var email = document.forms["RegForm"]["EMail"];
+ 
+   var password = document.forms["RegForm"]["Password"];
+
+ 
+var passWord = document.forms["RegForm"]["CPassword"];
+
+
+
+   if (name.value === "") 
+{
+ 
+window.alert("Please enter your name.");
+   
+     name.focus();
+    
+    return false;
+   
+ }
+
+  
+  if (email.value === "") 
+{
+        window.alert("Please enter a valid e-mail address.");
+    
+    email.focus();
+    
+    return false;
+    }
+
+ 
+   if (email.value.indexOf("@", 0) < 0) 
+{
+        window.alert("Please enter a valid e-mail address.");
+ 
+       email.focus();
+   
+     return false;
+    }
+
+   
+
+ if (email.value.indexOf(".", 0) < 0)
+ {
+        window.alert("Please enter a valid e-mail address.");
+ 
+       email.focus();
+
+        return false;
+    }
+
+  
+  if (password.value === "")
+
+ {
+        window.alert("Please enter your password");
+   
+     password.focus();
+    
+
+    return false;
+  
+  }
+
+   
+ return true;
+}
+
+if (passWord.value === "")
+
+ {
+        window.alert("Please enter your password");
+   
+     passWord.focus();
+    
+
+    return false;
+  
+  }
+
+   
+ return true;
+}
+
+
+
